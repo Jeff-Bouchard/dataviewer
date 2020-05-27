91d38f40178ec90dffca9989690ab16089c7caef
Andreas Kreiseder <kreiseder.a@gmail.com>
2020-05-28 00:15:39 +0200

    update docu

--- UploadLib.gcomp/javascript/upload.js
+++ UploadLib.gcomp/javascript/upload.js
@@ -1,28 +1,11 @@
-// Use the built-in browser dialog box to select and then open file.
-//
-// This example creates a function literal, and adds
-// the function as a property of the Window object.
-// The functions are defined within an anonymous function ( (function(){}()); )
-// as a closure to prevent polluting the global namespace with our variables.
-// If 'window.' is removed from 'window.OpenFileWebVI' then the function
-// is no longer accessible from the WebVI. This is because it is only in the scope of
-// this anonymous function, and not the global scope of the browser.
-//
-// From more information see:
-// https://github.com/ni/webvi-examples
-// https://developer.mozilla.org/en-US/docs/Web/API/Window
-
 var fileText ="";
 var fileRead = false;	
 var inputElementName = "myFile"; 
 
 (function () {
-// Use strict prevents silent and common JavaScript errors.
 'use strict';		
 window.OpenFileWebVI = function () {
-    console.log("start")
-    // For debugging, uncomment this line.
-    //console.log("in openfilewebti");
+ 
     document.getElementById(inputElementName).click();
 
     document.getElementById(inputElementName).onchange = function (){
@@ -34,14 +17,13 @@ window.OpenFileWebVI = function () {
 window.LoadFile = function () {
     var myFile = document.getElementById(inputElementName).files[0];
     var reader = new FileReader();
-    console.log(myFile)
 
     reader.addEventListener('load', function (e) {
       fileRead = false;
       fileText = e.target.result;
     });
-    
-     reader.readAsBinaryString(myFile);
+
+    reader.readAsBinaryString(myFile);
      reader.onload = function(e){
         fileRead = true;
      };
@@ -54,16 +36,14 @@ window.CheckIfDone = function (){
 window.ReadText = function (){
     fileRead = false;
 
-
     var snippet = fileText
     var button = document.getElementById("save-trigger")
 
-    //button.classList.add("loading")
     var blob = new Blob(
       [
-        "<html><head><title>Welcome To Skybin</title><head><body><pre>" +
-        snippet +
-        "</pre></body></html>"
+        //"<html><head><title>Test</title><head><body><pre>" +
+        snippet//+
+       // "</pre></body></html>"
       ],
       { type: "text/html" }
     )
@@ -76,8 +56,9 @@ window.ReadText = function (){
       uuid += cs.charAt(Math.floor(Math.random() * cs.length));
     }
 
+
     
-    fetch(`https://siasky.net/skynet/skyfile/${uuid}?filename=skybin.html`, {
+    fetch(`https://siasky.net/skynet/skyfile/${uuid}?filename=data.txt`, {
       method: "POST",
       body: formData
     })
@@ -85,30 +66,22 @@ window.ReadText = function (){
       .then(result => {
         //button.classList.remove("loading")
         var newLink = document.createElement("a")
-
+        console.log(formData)
         console.log(result.skylink)
         
-        newLink.href = result.skylink
-        newLink.classList.add("skylink")
-        newLink.setAttribute("target", "_blank")
-        newLink.textContent = "https://" + "siasky.net" + "/" + result.skylink
-        //"https://" + window.location.hostname + "/" + result.skylink
+        //newLink.href = result.skylink
+        //newLink.classList.add("skylink")
+        //newLink.setAttribute("target", "_blank")
+        //newLink.textContent = "https://" + "siasky.net" + "/" + result.skylink
 
-          
-        document.getElementById("upload_skylink").appendChild(newLink)
-        var linkBreak = document.createElement("br")
-        document.getElementById("upload_skylink").appendChild(linkBreak)
-        //document.getElementById("skylinks").appendChild(linkBreak)
-
+        var skylink = document.getElementById("upload_skylink")
+        skylink.setAttribute('text', "https://" + "siasky.net" + "/" + result.skylink )
+        skylink.setAttribute('href', "https://" + "siasky.net" + "/" + result.skylink )
       })
       .catch(error => {
-        button.classList.remove("loading")
         console.error("Error:", error)
-      })
-
-      return "done"
-    
+      }) 
+      return "uploading file..."
 }
-
 }());
 
